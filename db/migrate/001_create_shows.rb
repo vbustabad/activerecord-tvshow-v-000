ae4c743104859c0854a820cfbb10fac6ecfdde51
Veronica Bustabad <vbustabad@gmail.com>
2018-06-30 17:29:07 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,11 @@
-class Show
+class Show < ActiveRecord::Migration
 
   def change
     create_table :shows do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 
